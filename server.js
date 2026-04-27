450db02e8fa7756c52eb0a1cf94f5621450ed41d
amadoudev12 <delhadjamadou670@gmail.com>
2026-04-27 21:40:52 +0200

    jespere que ces le dernier

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const activerTrimestreAutomatique = require('./utils/activerTrimestre')
 const cors = require('cors')
 const app = express()
-const port = 5000
+const port = process.env.PORT
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
